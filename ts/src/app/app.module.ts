67d2f17905e9dd0b5c2f78b9fa71dcca20ff87d1
davidson <dehpacifico@gmail.com>
2024-04-27 18:12:32 -0300

    concluindo header e separando como componente

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ColunaSlaca2019Component } from './coluna-slaca2019/coluna-slaca2019.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ColunaSlaca2019Component
+    ColunaSlaca2019Component,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
